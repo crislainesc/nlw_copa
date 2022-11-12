333f5c4d1b73a0e21d39bfed992b53d6be34a890
crislainesc <criis.santos@outlook.com>
2022-11-12 17:32:36 -0300

    ✨ setup of app

---
+++ mobile/App.tsx
@@ -0,0 +1,30 @@
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { Loading } from '@/components'
+import { SingIn } from '@/screens'
+import { THEME } from '@/styles'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SingIn /> : <Loading />}
+    </NativeBaseProvider>
+  )
+}
