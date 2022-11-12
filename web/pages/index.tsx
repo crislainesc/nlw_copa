1c09ccc9feb099c03c53d5d2618d8ceca3aebef0
crislainesc <criis.santos@outlook.com>
2022-11-12 16:26:14 -0300

    ✨ setup of front end

---
+++ web/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+interface HomeProps {
+  count: number
+}
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count}</h1>
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      count: data.count,
+    },
+  }
+}
